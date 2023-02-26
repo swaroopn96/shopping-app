177b7444b9a9e12477a78da48e48c2a2494eb0a9
Swaroop N <swaroopn96@gmail.com>
2023-02-26 16:29:07 +0530

    Passed dynamic parameters to link

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -10,6 +10,7 @@ import { RecipeService } from '../../recipe.service';
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
   //@Output() recipeSelected = new EventEmitter<void>();
+  @Input() index: number;
 
   constructor(private recipeService: RecipeService) {}
 
